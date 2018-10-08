cd0a6804a65af0d485342a9bb13aba1320a901b7
dnoc <drew.noct@gmail.com>
2018-10-08 15:28:32 -0400

    initial test

--- src/main/ArrayAddition.groovy
+++ src/main/ArrayAddition.groovy
@@ -1,2 +1,10 @@
 class ArrayAddition {
+
+    /**
+     * Function that returns true if any combination of numbers in the array (excluding the largest number) can be
+     * added up to equal the largest number in the array, otherwise false
+     */
+    static boolean run() {
+        return false
+    }
 }
--- src/test/ArrayAdditionSpec.groovy
+++ src/test/ArrayAdditionSpec.groovy
@@ -2,4 +2,12 @@ import spock.lang.*
 
 class ArrayAdditionSpec extends Specification {
 
+
+    def "it always returns false"() {
+        when:
+        boolean result = ArrayAddition.run()
+
+        then:
+        !result
+    }
 }
