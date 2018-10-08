e75acb675e40c62f4a06da547a10f68525ace559
dnoc <drew.noct@gmail.com>
2018-10-08 15:35:11 -0400

    Rename, handle input

--- src/main/ArrayAddition.groovy
+++ src/main/ArrayAddition.groovy
@@ -3,8 +3,10 @@ class ArrayAddition {
     /**
      * Function that returns true if any combination of numbers in the array (excluding the largest number) can be
      * added up to equal the largest number in the array, otherwise false
+     *
+     * Prompt: https://www.coderbyte.com/editor/guest:Array%20Addition:Java
      */
-    static boolean run() {
-        return false
+    static boolean isAddable(int[] array) {
+        return array.length > 0
     }
 }
--- src/test/ArrayAdditionSpec.groovy
+++ src/test/ArrayAdditionSpec.groovy
@@ -3,11 +3,25 @@ import spock.lang.*
 class ArrayAdditionSpec extends Specification {
 
 
-    def "it always returns false"() {
+    def "it always returns false on empty array"() {
+        setup:
+        int[] array = new int[0]
+
         when:
-        boolean result = ArrayAddition.run()
+        boolean result = ArrayAddition.isAddable(array)
 
         then:
         !result
+    }
+
+    def "it always returns true on non-empty array"() {
+        setup:
+        int[] array = [0]
+
+        when:
+        boolean result = ArrayAddition.isAddable(array)
+
+        then:
+        result
     }
 }
