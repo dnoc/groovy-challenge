66e6adf190587ad8e610410eae54e84018fb75a5
dnoc <drew.noct@gmail.com>
2018-10-09 09:38:04 -0400

    Algorithm

--- src/main/ArrayAddition.groovy
+++ src/main/ArrayAddition.groovy
@@ -7,6 +7,28 @@ class ArrayAddition {
      * Prompt: https://www.coderbyte.com/editor/guest:Array%20Addition:Java
      */
     static boolean isAddable(int[] array) {
-        return array.length > 0
+        int length = array.length
+        if (length < 2) return false
+
+        int[] sorted = Arrays.sort(array)
+        int largestVal = sorted[length]
+
+        return findSum(Arrays.copyOfRange(sorted, 0, length-1), length-1, largestVal)
+    }
+
+    private static boolean findSum(int[] array, int length, int sum) {
+        if (sum == 0) {
+            return true
+        }
+
+        if (length == 0 && sum != 0) {
+            return false
+        }
+
+        if (array[length-1] > sum) {
+            return findSum(array, length-1, sum)
+        }
+
+        return findSum(array, length-1, sum) || findSum(array, length-1, sum-array[length-1])
     }
 }
