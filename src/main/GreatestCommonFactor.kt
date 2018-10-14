2dfffd9cc2cbe61adff7a5d7ddc089edf7a027f8
dnoc <drew.noct@gmail.com>
2018-10-13 22:12:51 -0400

    Throw a little Euclid in there

--- src/main/GreatestCommonFactor.kt
+++ src/main/GreatestCommonFactor.kt
@@ -1,5 +1,14 @@
 class GreatestCommonFactor {
-    fun find() : Int {
-        return 0
+    /**
+     * Prompt: https://www.coderbyte.com/editor/guest:Division:Java
+     */
+    fun find(a : Int, b : Int) : Int {
+        if (a > b) {
+            return find(a-b, b)
+        } else if (a < b) {
+            return find (a, b-a)
+        } else {
+            return a
+        }
     }
 }
--- src/test/GreatestCommonFactorSpec.groovy
+++ src/test/GreatestCommonFactorSpec.groovy
@@ -3,11 +3,18 @@ import spock.lang.Specification
 class GreatestCommonFactorSpec extends Specification {
     def gfc = new GreatestCommonFactor()
 
-    def "it returns a number"() {
-        given:
-        int result = gfc.find()
-
+    def "it returns the greatest common factor"(int a, int b, int expected) {
         expect:
-        result == 0
+        gfc.find(a, b) == expected
+
+        where:
+        a       | b     | expected
+        7       | 3     | 1
+        36      | 54    | 18
+        20      | 50    | 10
+        20      | 25    | 5
+        252     | 105   | 21
+        13000   | 52    | 52
+        1       | 1     | 1
     }
 }
