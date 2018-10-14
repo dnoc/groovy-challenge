c90e502ac2799a828f2904247e56e0be9673079d
dnoc <drew.noct@gmail.com>
2018-10-13 22:14:55 -0400

    fancy 'when' notation

--- src/main/GreatestCommonFactor.kt
+++ src/main/GreatestCommonFactor.kt
@@ -3,14 +3,11 @@ class GreatestCommonFactor {
      * Prompt: https://www.coderbyte.com/editor/guest:Division:Java
      */
     fun find(a : Int, b : Int) : Int {
-        if (a == 0 || b == 0) {
-            return 0
-        } else if (a > b) {
-            return find(a-b, b)
-        } else if (a < b) {
-            return find (a, b-a)
-        } else {
-            return a
+        return when {
+            a == 0 || b == 0 -> 0
+            a > b -> find(a-b, b)
+            a < b -> find (a, b-a)
+            else -> a
         }
     }
 }
